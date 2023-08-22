4713ce1f33e6619691b57cc1802bdd2902f47559
Tommyk <tomm8231@stud.kea.dk>
2023-08-22 13:06:25 +0200

    Objects and Arrays exercises done

---
+++ 00._Miscellaneous/01._Intro_Exercises/03.Objects.js
@@ -0,0 +1,49 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = { message: "Hello, earthling! I bring peace." };
+
+// Log the message 
+
+console.log(myObj.message)
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+
+const person = {
+    "name" : "Tommy",
+    "age" : 32
+}
+console.log(person)
+
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {};
+
+// make a rule called isAllowed and let the value be true
+
+stackOverflow.isAllowed = true
+
+console.log(stackOverflow)
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = { description: "The best song in the world." };
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+delete thisSong.description
+
+console.log(thisSong)
+
+thisSong.about = "Just a tribute"
+
+console.log(thisSong)
+
+// --------------------------------------
---
+++ 00._Miscellaneous/01._Intro_Exercises/04.Arrays.js
@@ -0,0 +1,101 @@
+// --------------------------------------
+// Arrays, for loops
+// --------------------------------------
+// Exercise 1 - Array Positioning
+
+const letters = ["a","b","c"];
+// show b in the console 
+
+console.log(letters[1])
+
+
+// --------------------------------------
+// Exercise 2 - Array Positioning
+
+// What a lonely array. Add at least 3 friend objects to it.  
+const friends = [];
+friends.push("friend1", "friend2", "friend3")
+console.log(friends)
+
+
+// --------------------------------------
+// Exercise 3 - Get the index of first occurance of that value. 
+
+const significantMathNumbers = [0, 2.718, 3.14159, 1729];
+
+// You want to programmatically find where the number 1729 is in the array.
+// programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+console.log(significantMathNumbers.indexOf(1729,0))
+
+
+
+// --------------------------------------
+// Exercise 4 - Inserting elements
+
+const diet = ["tomato", "cucumber", "rocket"];
+
+// You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
+
+diet.splice(2,0,"hamburger", "soda", "pizza")
+
+console.log(diet)
+
+
+
+// --------------------------------------
+// Exercise 5 - Remove element
+
+// Remove the LAST element of the array.
+// Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
+
+diet.pop()
+console.log(diet)
+
+
+// --------------------------------------
+// Exercise 6 - Copy array
+
+// You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.
+
+copyOfDiet = diet.map((ingredient) => ingredient)
+
+console.log(copyOfDiet)
+
+
+
+// --------------------------------------
+// Exercise 7 - For loop
+
+const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
+
+// log every second char in the array starting from b
+
+const startIndex = lettersExpanded.indexOf("b")
+
+for(let i = startIndex; i<lettersExpanded.length; i += 2) {
+    console.log(lettersExpanded[i])
+}
+
+
+
+// --------------------------------------
+// Exercise 8 - For loop and if statement
+
+const numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
+
+const discardedNumbers = [];
+
+// log the element if the number is above 6 or below 0
+// else push them to the array discardedNumbers
+
+numbers.forEach(element => {
+    if(element>6 || element<0) {
+        console.log(element)
+    } else {
+        discardedNumbers.push(element)
+    }
+});
+
+console.log(discardedNumbers)
+
+// --------------------------------------
