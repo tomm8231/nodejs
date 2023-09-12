3b2fdd382b3d28e3812cc21d685b39287d7e655c
Tommyk <tomm8231@stud.kea.dk>
2023-09-12 14:07:50 +0200

    refactoring

--- assignment_time/app.js
+++ assignment_time/app.js
@@ -13,6 +13,3 @@ app.get("/whatstime", (req, res) => {
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
 })
-
-
-//Hej Gitte. Lige et opfølgende spørgsmål vedr praktik. Mit praktikmål ligger i Nuuk - hvilket jeg gerne vil holde mellem os, da jeg ikke vil plante ideen om Grønland hos de andre. Mit spørgsmål er om Grønland går under EØS og om der kan drages på de økonomiske muligheder, der er, hvis man tager til et andet land EU-land?
