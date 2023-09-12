0ceb105b21e99dc533fb243f45af4d8acef143cf
Tommyk <tomm8231@stud.kea.dk>
2023-09-12 15:40:21 +0200

    new endpoint for home

--- assignment_time/app.js
+++ assignment_time/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
 
